da1ad3fb90174929ff3671cba11c297d80effcc6
parshyagk23 <gkprashant52@gmail.com>
2024-01-26 16:01:34 +0530

    added registerfrom added validation and navigation and localstorage

---
+++ src/Components/RegisterForm.jsx
@@ -0,0 +1,240 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
+const RegisterForm = () => {
+  const [formValues, SetfromValues] = useState({
+    name: "",
+    username: "",
+    email: "",
+    mobile: "",
+    registerbox: false,
+  });
+  const [errors, setErrors] = useState({
+    name: null,
+    username: null,
+    email: null,
+    mobile: null,
+    registerbox: null,
+  });
+  const navigate= useNavigate();
+  const handleSubmit = () => {
+    let isErrors = false;
+    if (formValues.name.trim().length == 0) {
+      setErrors((prev) => {
+        return { ...prev, name: "Name field is required" };
+      });
+      isErrors=true;
+      
+    } else {
+      setErrors((prev) => {
+        return { ...prev, name: null };
+      });
+    }
+    if (formValues.username.trim().length == 0) {
+      setErrors((prev) => {
+        return { ...prev, username: "Username field is required" };
+      });
+      isErrors=true;
+    } else {
+      setErrors((prev) => {
+        return { ...prev, username: null };
+      });
+    }
+    if (formValues.email.trim().length == 0) {
+      setErrors((prev) => {
+        return { ...prev, email: "Email field is required" };
+      });
+      isErrors=true;
+    } else {
+      setErrors((prev) => {
+        return { ...prev, email: null };
+      });
+    }
+    if (formValues.mobile.trim().length == 0) {
+      setErrors((prev) => {
+        return { ...prev, mobile: "Mobile field is required" };
+      });
+      isErrors=true;
+    } else {
+      setErrors((prev) => {
+        return { ...prev, mobile: null };
+      });
+    }
+    if (formValues.registerbox === false) {
+      setErrors((prev) => {
+        return { ...prev, registerbox: "Please check the box" };
+      });
+      isErrors=true;
+    } else {
+      setErrors((prev) => {
+        return { ...prev, registerbox: null };
+      });
+    }
+    if(isErrors){
+      (true)
+      return;
+    }else{
+      window.localStorage.setItem("userInfo", JSON.stringify(formValues));
+      navigate('/Info');
+    }
+  };
+  return (
+    <FormData>
+      <div>
+        <input
+          type="text"
+          name="name"
+          value={formValues.name}
+          placeholder="Name"
+          onChange={(e) => {
+            SetfromValues((prev) => {
+              return { ...prev, name: e.target.value };
+            });
+          }}
+        />
+        <p>{errors.name}</p>
+      </div>
+      <div>
+        <input
+          type="text"
+          name="username"
+          value={formValues.username}
+          placeholder="UserName"
+          onChange={(e) => {
+            SetfromValues((prev) => {
+              return { ...prev, username: e.target.value };
+            });
+          }}
+        />
+        <p>{errors.username}</p>
+      </div>
+      <div>
+        <input
+          type="text"
+          name="email"
+          value={formValues.email}
+          placeholder="Email"
+          onChange={(e) => {
+            SetfromValues((prev) => {
+              return { ...prev, email: e.target.value };
+            });
+          }}
+        />
+        <p>{errors.email}</p>
+      </div>
+      <div>
+        <input
+          type="text"
+          name="mobile"
+          value={formValues.mobile}
+          placeholder="Mobile"
+          onChange={(e) => {
+            SetfromValues((prev) => {
+              return { ...prev, mobile: e.target.value };
+            });
+          }}
+        />
+        <p>{errors.mobile}</p>
+      </div>
+      <div >
+        <div className="check">
+        <input
+          type="checkbox"
+          name="registerbox"
+          id="registerbox"
+          placeholder="Name"
+          value={formValues.registerbox}
+          onChange={(e) => {
+            SetfromValues((prev) => {
+              return { ...prev, registerbox: e.target.checked };
+            });
+          }}
+        />
+        <label htmlFor="registerbox">
+          Share my registration data with Superapp
+        </label>
+        </div>
+        <p className="checkerr" > {errors.registerbox} </p>
+      </div>
+
+      <button onClick={handleSubmit} type="submit">
+        Sign up
+      </button>
+    </FormData>
+  );
+};
+
+export default RegisterForm;
+
+const FormData = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 21px;
+
+  div {
+    display: flex;
+    flex-direction: column;
+   
+  }
+
+  input {
+    width: 518px;
+    height: 55px;
+    outline: none;
+    border: none;
+    border-radius: 4px;
+    background: #292929;
+    padding: 20px;
+    font-family: "DM Sans";
+    font-size: 21px;
+    font-weight: 400;
+    color: #7c7c7c;
+  }
+  div p {
+    color: #f00;
+    font-family: "DM Sans";
+    font-size: 20px;
+    font-weight: 300;
+    line-height: 27px;
+    letter-spacing: 0.4px;
+  }
+  .check {
+    display: flex;
+    flex-direction: row;
+    align-self: baseline;
+    gap:10px;
+    
+  }
+   
+  .checkerr {
+    color: #f00;
+    font-family: "DM Sans";
+    font-size: 20px;
+    font-weight: 300;
+    line-height: 27px;
+    letter-spacing: 0.4px;
+    width: 518px;
+  }
+  .check input {
+    width: 30px;
+    height: 20px;
+  }
+
+  button {
+    all: unset;
+    width: 518px;
+    height: 55px;
+    border-radius: 28px;
+    background: #72db73;
+    font-family: "Roboto";
+    font-size: 29px;
+    font-weight: 600;
+    line-height: 32px;
+    letter-spacing: 1.6px;
+    color: white;
+    text-align: center;
+    cursor: pointer;
+  }
+`;
