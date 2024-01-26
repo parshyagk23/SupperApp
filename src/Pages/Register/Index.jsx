05721b45b64ec493fce3aedbe1464702c5bfa840
parshyagk23 <gkprashant52@gmail.com>
2024-01-26 15:59:51 +0530

    added register form added validation and navigation

--- src/Pages/Register/Index.jsx
+++ src/Pages/Register/Index.jsx
@@ -1,25 +1,26 @@
 
 import styled from "styled-components"
-import RegisterForm from "./RegisterForm";
+import RegisterForm from "../../Components/RegisterForm";
+import { useState } from "react";
 
 const Register =()=>{
-    
+   
     return(
         <>
         <MainEle>
 
-        <Section1>
+        <Section1 >
             <div className="Rockimg">
             <h1>Discover new things on Superapp</h1>
             </div>
 
         </Section1>
-        <Section2>
+        <Section2 >
             <div className="AppTitle" >
                 <h1>Super App</h1>
                 <p>Create your new account</p>
             </div>
-           <RegisterForm/>
+           <RegisterForm />
             <div className="term-privacy">
                 <p>By clicking on Sign up. you agree to Superapp <span> Terms and Conditions of Use</span></p>
                 <p>To learn more about how Superapp collects, uses, shares and protects your personal data please head Superapp <span> Privacy Policy</span></p>
@@ -38,8 +39,8 @@ const MainEle = styled.main`
 `
 
 const Section1 =styled.section`
-    width: 640px;
-    height: 775px;
+    width: 600px;
+    
     background: url('images/Rock.png'), lightgray 0px -49.673px / 100% 113.358% no-repeat;
     .Rockimg{
         margin: 550px 84px 0px 62px;
@@ -58,7 +59,8 @@ const Section1 =styled.section`
 
 const Section2 =styled.section`
     color: white;
-    width: 640px;
+    width: 680px;
+    margin: 0 auto;
     
     .AppTitle{
         display: flex;
@@ -88,14 +90,14 @@ const Section2 =styled.section`
         text-align: left;
         width: 518px;
         margin: 0 auto;
-        margin-top: 27px;
+        margin-top: 25px;
         color:#7c7c7c;
         font-family:'Roboto' ;
         font-size: 18px;
         font-weight: 500;
         line-height: 26px;
         letter-spacing: .5px;
-        margin-bottom: 55px;
+        margin-bottom: 20px;
     }
     .term-privacy span{
         color: #72DB73;
